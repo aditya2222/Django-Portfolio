44fed5f47b59a50773ae9f2eae41d19617dd216f
adityasingh222247@gmail.com <adityasingh222247@gmail.com>
2018-07-21 22:11:39 +0530

    static commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,12 +131,8 @@ USE_TZ = True
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Configure Django App for Heroku.
 # import django_heroku
 # django_heroku.settings(locals())
-
 
